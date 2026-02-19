34537e628847abd8004b842aecde790cab788d28
teevik <teemu.vikoren@gmail.com>
2026-02-19 22:57:13 +0100

    passwords

--- modules/nixos/standard.nix
+++ modules/nixos/standard.nix
@@ -4,6 +4,12 @@
   config,
   ...
 }:
+let
+  initialHashedPassword = "$6$X19Q8OhBkw8xUegs$prAFssd1NxBR1qrdMUhqZX4Xqy02bTeNfCZw24YCMClQhp8Pox65w6PF5w7hV2foKfGytsXTwCB5pQ7FLwF7o/";
+  sshKeys = [
+    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC3/B6lqoz+fG6QqerYS4kMCtrgl+hCvN6OhuXgcG9yCf0QhRfPFuPGqq/X5DgmyZsLAheH5YgaXxmtPnfO3+t4k9JEFgDRr8SB3dDxzfoQJfHi7SGk1dyjJ9CGgTzrc9Kgs1Zvp76NAug3VkPHv+J92oytprFEiBRzLg0HV8NWtFkDiR0TtjLbxt28UAHyD1v0VjjC9WRuzRPiiQonye5Tk3fz6Z9PGItWht+3Jhnvhb6CHxHcio8582w9xRpP9Ho+yFYINgipQDfU2MDcZxIaqnM4tWS0ExTdneIWqWcmP28EyWWQo0DayY/lxp2WIL/AbQ94bjCSzlgy29KVAU13T2ct/m70VGo8xqB3cBhd/bhIQlamGc5D8pS2XipRmPXrdMGW4tkr9k4La0SOkS1kb47solf5K1AXtMgslFh7844+mQbMfYVRbnAtjOMtZjsOmzOwxtVaZLdmYLPSgTZO0a92UXyZRd8/apx+erg+Oak9pHHfp4rZi86/sUue8d8= teemu@DESKTOP-J8H46CI"
+  ];
+in
 {
   # Boot
   boot.loader.systemd-boot.enable = true;
@@ -42,15 +48,19 @@
   sops.secrets.tailscale_key = { };
 
   # System user
+  users.users.root = {
+    inherit initialHashedPassword;
+    openssh.authorizedKeys.keys = sshKeys;
+  };
+
   users.users.teevik = {
     isNormalUser = true;
+    inherit initialHashedPassword;
     extraGroups = [
       "wheel"
       "networkmanager"
     ];
-    openssh.authorizedKeys.keys = [
-      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC3/B6lqoz+fG6QqerYS4kMCtrgl+hCvN6OhuXgcG9yCf0QhRfPFuPGqq/X5DgmyZsLAheH5YgaXxmtPnfO3+t4k9JEFgDRr8SB3dDxzfoQJfHi7SGk1dyjJ9CGgTzrc9Kgs1Zvp76NAug3VkPHv+J92oytprFEiBRzLg0HV8NWtFkDiR0TtjLbxt28UAHyD1v0VjjC9WRuzRPiiQonye5Tk3fz6Z9PGItWht+3Jhnvhb6CHxHcio8582w9xRpP9Ho+yFYINgipQDfU2MDcZxIaqnM4tWS0ExTdneIWqWcmP28EyWWQo0DayY/lxp2WIL/AbQ94bjCSzlgy29KVAU13T2ct/m70VGo8xqB3cBhd/bhIQlamGc5D8pS2XipRmPXrdMGW4tkr9k4La0SOkS1kb47solf5K1AXtMgslFh7844+mQbMfYVRbnAtjOMtZjsOmzOwxtVaZLdmYLPSgTZO0a92UXyZRd8/apx+erg+Oak9pHHfp4rZi86/sUue8d8= teemu@DESKTOP-J8H46CI"
-    ];
+    openssh.authorizedKeys.keys = sshKeys;
   };
 
   # Nix settings
