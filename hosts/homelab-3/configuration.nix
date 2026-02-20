f4340dc910074606fd280dfb7c27398b9bcf1673
teevik <teemu.vikoren@gmail.com>
2026-02-20 01:33:15 +0100

    quiet mode

--- hosts/homelab-3/configuration.nix
+++ hosts/homelab-3/configuration.nix
@@ -25,6 +25,29 @@
   # ASUS ROG - disable keyboard LEDs and AniMe Matrix
   services.asusd = {
     enable = true;
+    asusdConfig = {
+      text = ''
+        (
+            charge_control_end_threshold: 100,
+            base_charge_control_end_threshold: 100,
+            disable_nvidia_powerd_on_battery: true,
+            ac_command: "",
+            bat_command: "",
+            platform_profile_linked_epp: true,
+            platform_profile_on_battery: Quiet,
+            change_platform_profile_on_battery: true,
+            platform_profile_on_ac: Quiet,
+            change_platform_profile_on_ac: true,
+            profile_quiet_epp: Power,
+            profile_balanced_epp: BalancePower,
+            profile_custom_epp: Performance,
+            profile_performance_epp: Performance,
+            ac_profile_tunings: {},
+            dc_profile_tunings: {},
+            armoury_settings: {},
+        )
+      '';
+    };
     animeConfig = {
       text = ''
         (
