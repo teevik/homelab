306760011f326de6d0ab85364c03a54004ec0ef7
teevik <teemu.vikoren@gmail.com>
2026-03-03 15:44:59 +0100

    nob

--- kubernetes/paperless-ngx.nix
+++ kubernetes/paperless-ngx.nix
@@ -20,8 +20,8 @@ let
       key = "PAPERLESS_SECRET_KEY";
     };
     PAPERLESS_TIME_ZONE.value = "Europe/Oslo";
-    PAPERLESS_OCR_LANGUAGES.value = "nob";
-    PAPERLESS_OCR_LANGUAGE.value = "eng+nob";
+    PAPERLESS_OCR_LANGUAGES.value = "nor";
+    PAPERLESS_OCR_LANGUAGE.value = "eng+nor";
     PAPERLESS_URL.value = "http://paperless";
     PAPERLESS_ADMIN_USER.value = "admin";
     PAPERLESS_ADMIN_PASSWORD.value = "admin";
